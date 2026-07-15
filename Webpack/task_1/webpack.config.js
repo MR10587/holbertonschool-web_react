9652a93ef8fce0dcc943170d92dd32705734051a
MR10587 <ramalmirzezade20@gmail.com>
2026-07-15 18:44:41 +0400

    .

---
+++ Webpack/task_1/webpack.config.js
@@ -0,0 +1,13 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+};
