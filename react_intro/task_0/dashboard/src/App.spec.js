44546671b28930387ac951300d0a2db164af20b6
MR10587 <ramalmirzezade20@gmail.com>
2026-07-20 23:05:29 +0400

    .

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App.jsx";
 
-describe(App, () => {
-  render(<App />);
+describe("App", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
 
   it("h1 element with the text School Dashboard is rendered.", () => {
-    expect(screen.getByText("School Dashboard").toBeInTheDocument());
+    expect(screen.getByText("School dashboard")).toBeInTheDocument();
   });
 
   it("renders the correct text in the body and footer paragraphs", () => {
-    render(<App />);
-
     const bodyParagraph = screen.getByText(
       "Login to access the full dashboard",
     );
@@ -29,6 +29,6 @@ describe(App, () => {
   });
 
   it("image is rendered", () => {
-    expect(screen.getByAltText("holberton logo").toBeInTheDocument());
+    expect(screen.getByAltText("holberton logo")).toBeInTheDocument();
   });
 });
