1dc72e2ef4fde2dbe0dbffaec14b6428203b03de
MR10587 <ramalmirzezade20@gmail.com>
2026-07-20 13:28:32 +0400

    .

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import holbertonLogo from './assets/holberton-logo.jpg'
-import './App.css'
+import { useState } from "react";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import holbertonLogo from "./assets/holberton-logo.jpg";
+import "./App.css";
 
 function App() {
   return (
     <>
       <div className="App-header">
-        <img src={holbertonLogo} alt="holberton logo"width="200px"></img>
+        <img src={holbertonLogo} alt="holberton logo" width="200px"></img>
         <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>Copyright {new Date().getFullYear()} - holberton School</p>
+        <p>copyright {new Date().getFullYear()} - Holberton School</p>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
