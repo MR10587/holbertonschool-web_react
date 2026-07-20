87a25d33e4f3ae3e9b2b2861139b75264587a1f9
MR10587 <ramalmirzezade20@gmail.com>
2026-07-20 21:35:42 +0400

    .

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import holbertonLogo from "./assets/holberton-logo.jpg";
 import "./App.css";
 
 function App() {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <>
+    <div className="App">
       <div className="App-header">
-        <img src={holbertonLogo} alt="holberton logo" width="200px"></img>
-        <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
       <div className="App-footer">
-        <p>copyright {new Date().getFullYear()} - Holberton School</p>
+        <p>Copyright {currentYear} - holberton School</p>
       </div>
-    </>
+    </div>
   );
 }
 
