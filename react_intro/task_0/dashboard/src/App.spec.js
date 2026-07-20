7dbf76b92ebeb8946d69e176f3a26efc885c2b4b
MR10587 <ramalmirzezade20@gmail.com>
2026-07-20 23:09:18 +0400

    .

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -1,34 +1,30 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App.jsx";
+import App from "./App";
 
 describe("App", () => {
-  beforeEach(() => {
+  it("renders the heading", () => {
     render(<App />);
+
+    expect(screen.getByText(/School dashboard/i)).toBeInTheDocument();
   });
 
-  it("h1 element with the text School Dashboard is rendered.", () => {
-    expect(screen.getByText("School dashboard")).toBeInTheDocument();
+  it("renders the correct body and footer text", () => {
+    render(<App />);
+
+    expect(
+      screen.getByText("Login to access the full dashboard"),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText(
+        new RegExp(`Copyright ${new Date().getFullYear()} - holberton School`),
+      ),
+    ).toBeInTheDocument();
   });
 
-  it("renders the correct text in the body and footer paragraphs", () => {
-    const bodyParagraph = screen.getByText(
-      "Login to access the full dashboard",
-    );
+  it("renders the logo image", () => {
+    render(<App />);
 
-    const footerParagraph = screen.getByText(
-      new RegExp(`Copyright ${new Date().getFullYear()} - holberton School`),
-    );
-
-    expect(bodyParagraph).toHaveTextContent(
-      "Login to access the full dashboard",
-    );
-
-    expect(footerParagraph).toHaveTextContent(
-      `Copyright ${new Date().getFullYear()} - holberton School`,
-    );
-  });
-
-  it("image is rendered", () => {
     expect(screen.getByAltText("holberton logo")).toBeInTheDocument();
   });
 });
